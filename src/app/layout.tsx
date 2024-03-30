de6d1f3d12f147efcada5d2d98d410b5a6a793af
Leonardo luis rampazi <105321172+Leonardo-Rampazi@users.noreply.github.com>
2024-03-30 15:44:40 -0300

    atualizando diretorio

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+export const metadata: Metadata = {
+  title: 'CadastrodePessoas',
+  description: 'Cadastro de pessoas em next.js',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.variable}>{children}</body>
+    </html>
+  )
+}
